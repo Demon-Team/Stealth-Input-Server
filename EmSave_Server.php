5cccfba75aebfe90716e0b2c282993bca892e00d
Demon-Team <thenonexistent666@gmail.com>
2016-06-07 18:32:34 +0430

    Update EmSave_Server.php
    -Password Protection
    -Dynamic Address And Port
    -Configuration File
    -Fixed Bugs

--- EmSave_Server.php
+++ EmSave_Server.php
@@ -2,64 +2,144 @@
 #Coded By The Nonexistent.
 #D3M0N Team.
 #You Can Connect From NetCat To The Script.
-#run: php -q script.php
-$host = "0.0.0.0";
-$port = 1246;
-$welcome_message = "[+]Welcome To PHP Emergency Save Service.\n[+]Everything You Input Will Be Hash Saved And The Session Will Leave No Log.\n[+]Stealth Session Started\n[+]Input:\n";
-$isshut = FALSE;
-set_time_limit(0);
-echo "[Emergency Save Server]\n";
-while(true)
+#run: php -q script.php ip port
+#input any for any ip
+if (isset($argv[1]) && isset($argv[2]))
 {
-	$lsock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-	socket_bind($lsock, $host, $port);
-	socket_listen($lsock, 3);
-	echo "\n";
-	echo "[+]Waiting For Connection.\n";
-	$accsock = socket_accept($lsock);
-	socket_getpeername($accsock, $caddress, $cport);
-	echo "[+]Recived Connection Request From : $caddress  \n";
-	socket_write($accsock,$welcome_message ,strlen($welcome_message));
-	echo "[+]Welcome Message Sent.\n";
-	$handle = fopen("stealth_log.txt", "a");
-	fwrite($handle, "Connection From : $caddress \n", strlen("Connection From : $caddress \n"));
-	do
+	$argv_set = true;
+	$host = $argv[1];
+	if ($host == "any")
 	{
-		$input = socket_read($accsock, 1024);
-		$input = trim($input);
-		if ($input == "exit")
+		$outhost = "0.0.0.0";
+	}
+	else
+	{
+		$outhost = $host;
+	}
+}
+else
+{
+	$argv_set = false;
+}
+$config_file = "Em.conf";
+if (!file_exists($config_file))
+{
+	echo "[Emergency Save Server]\n";
+	echo "[#]Config File Not Found!\n[+]Started Generating\n";
+	$password = readline("[+]Enter Server's Password[MAX:2048B]: ");
+	$password = md5($password);
+	$name = readline("[+]Enter Server's Name: ");
+	$outputfile = readline("[+]Enter Output File's Name: ");
+	echo "[+]Done!\n[+]Writing To The Config File\n";
+	$config_handle = fopen($config_file, "a");
+	fwrite($config_handle, $password . "\n", strlen($password . "\n"));
+	fwrite($config_handle, $name . "\n", strlen($name . "\n"));
+	fwrite($config_handle, $outputfile . "\n", strlen($outputfile . "\n"));
+	fclose($config_handle);
+	echo "[+]Restart The Script For Changes To Effect\n";
+	exit();
+}
+else
+{
+	if ($argv_set)
+	{
+	    //Getting Values From Config File
+		try
 		{
-			fwrite($handle, "[+]Exit Command Recived.\n[+]Socket Terminated.\n", strlen("[+]Exit Command Recived.\n[+]Socket Terminated.\n"));
-			echo "[+]Exit Command Recived.\n";
-		    socket_write($accsock, "[+]All Inputes Saved.\n", strlen("[+]All Iputes Saved.\n"));
-		    socket_write($accsock, "[+]Socket Terminated.\n", strlen("[+]Socket Terminated.\n"));
-			echo "[+]Terminating Socket.\n";
-			socket_close($accsock);
-			break;
+			$read_conf_handle = fopen($config_file, "r");
+			$password = fgets($read_conf_handle);
+			$password = trim($password);
+			$name = fgets($read_conf_handle);
+			$name = trim($name);
+			$outputfile = fgets($read_conf_handle);
+			$outputfile = trim($outputfile);
+			fclose($read_conf_handle);
 		}
-		elseif ($input == "server.shutdown")
+		catch (Exception $e)
 		{
+			echo "[#]Something Has Gone Wrong With The Config File.\n[+]Delete It And Start Again Or Try To Fix It\n";
+		}
+
+		echo "[+]Config Loaded Successfully.\n";
+		$port = $argv[2];
+		$welcome_message = "[+]Welcome To PHP Emergency Save Service.\n[+]Everything You Input Will Be Hash Saved And The Session Will Leave No Log.\n[+]Stealth Session Started\n[+]Input:\n";
+		$isshut = FALSE;
+		set_time_limit(0);
+		echo "[Emergency Save Server]\n";
+		while(true)
+		{
+			$lsock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+			socket_bind($lsock, $outhost, $port);
+			socket_listen($lsock, 3);
 			echo "\n";
-			echo "[+]Server Shutdown Signal Recived.\n[+]Shuting Down The Server.\n[+]All Connections Will Be Closed.\n";
-			socket_write($accsock, "[+]Server Shutdown Signal Recived.\n[+]All Inputes Saved.\n", strlen("[+]Server Shutdown Signal Recived.\n[+]All Inputes Saved.\n"));
-		    socket_write($accsock, "[+]Socket Terminated.\n", strlen("[+]Socket Terminated.\n"));
-			$isshut = TRUE;
-			break;
+			echo "[+]Host: " . $host . "\n";
+			echo "[+]Port: " . $port . "\n";
+			echo "[+]Waiting For Connection.\n";
+			$accsock = socket_accept($lsock);
+			socket_getpeername($accsock, $caddress, $cport);
+			echo "[+]Recived Connection Request From : $caddress  \n";
+			$handle = fopen($outputfile, "a");
+			fwrite($handle, "Connection From : $caddress \n", strlen("Connection From : $caddress \n"));
+			socket_write($accsock,"[+]Enter Serevr's Password: " ,strlen("[+]Enter Serevr's Password: "));
+			$input_password = $input = socket_read($accsock, 2048);
+			$input_password = trim($input_password);
+			if ($password == trim(md5($input_password)))
+			{
+				fwrite($handle, "Inputed Password Was Correct!\n", strlen("Inputed Password Was Correct!\n"));
+				echo "[+]Inputed Password Was Correct!\n";
+				socket_write($accsock,$welcome_message ,strlen($welcome_message));
+				echo "[+]Welcome Message Sent.\n";
+				do
+				{
+					$input = socket_read($accsock, 1024);
+					$input = trim($input);
+					if ($input == "exit")
+					{
+						fwrite($handle, "[+]Exit Command Recived.\n[+]Socket Terminated.\n", strlen("[+]Exit Command Recived.\n[+]Socket Terminated.\n"));
+						echo "[+]Exit Command Recived.\n";
+					    socket_write($accsock, "[+]All Inputes Saved.\n", strlen("[+]All Iputes Saved.\n"));
+					    socket_write($accsock, "[+]Socket Terminated.\n", strlen("[+]Socket Terminated.\n"));
+						echo "[+]Terminating Socket.\n";
+						socket_close($accsock);
+						break;
+					}
+					elseif ($input == "server.shutdown")
+					{
+						echo "\n";
+						echo "[+]Server Shutdown Signal Recived.\n[+]Shuting Down The Server.\n[+]All Connections Will Be Closed.\n";
+						socket_write($accsock, "[+]Server Shutdown Signal Recived.\n[+]All Inputes Saved.\n", strlen("[+]Server Shutdown Signal Recived.\n[+]All Inputes Saved.\n"));
+					    socket_write($accsock, "[+]Socket Terminated.\n", strlen("[+]Socket Terminated.\n"));
+						$isshut = TRUE;
+						break;
+					}
+					else
+					{
+						$write = "[+]User Input: " . $input . "\n";
+						echo $write;
+						fwrite($handle, $write, strlen($write));
+					} 
+				} while(true);
+				socket_close($lsock);
+				fclose($handle);
+				echo "[+]Socket Terminated.\n";
+				echo "\n";
+				if ($isshut)
+				{
+					break;
+				}
+			}
+			else
+			{
+				fwrite($handle, "Inputed Password Was Wrong!\nUser Disconnected.\n", strlen("Inputed Password Was Wrong!\nUser Disconnected.\n"));
+				echo "[+]Inputed Password Was Wrong!\n";
+				socket_close($lsock);
+				fclose($handle);
+			}
 		}
-		else
-		{
-			$write = "[+]User Input: " . $input . "\n";
-			echo $write;
-			fwrite($handle, $write, strlen($write));
-		} 
-	} while(true);
-	socket_close($lsock);
-	fclose($handle);
-	echo "[+]Socket Terminated.\n";
-	echo "\n";
-	if ($isshut)
+	}
+	else
 	{
-		break;
+		echo "[#]Arguments Error.\n[+]Check Your Input.\n";		
 	}
 }
 #Coded By The Nonexistent.
